acd1422a3fd449e84afef14be707f57c13054b27
Guilherme Lino <guilhermelinosp@gmail.com>
2022-12-21 06:17:46 -0300

    feat: init new project

---
+++ README.md
@@ -0,0 +1,35 @@
+# <div align="center"> APP Pizza Hub </div>
+
+</br>
+
+<div align="center">
+<p>🚧 It is in Development 🚧</p>
+</div>
+
+</br>
+
+## Technologies used in the project
+
+- Typescript
+- Expo
+- Axios
+
+## How to run the project
+
+- Clone this repository
+
+```shell
+git clone https://github.com/guilhermelinosx/app-pizza-hub.git
+```
+
+</br>
+
+- Start the Application in Development
+
+```shell
+yarn install && yarn start:dev
+```
+
+</br>
+
+- To stop the Application click CTRL+C in your terminal
---
+++ src/components/ListItems/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { styles } from './styles'
+
+import { Feather } from '@expo/vector-icons'
+
+interface ItemsProps {
+	data: {
+		id: string
+		product_id: string
+		name: string
+		amount: string | number
+	}
+	deleteItem: (item_id: string) => void
+}
+
+export function ListItems({ data, deleteItem }: ItemsProps) {
+	function handleDeleteItem() {
+		deleteItem(data.id)
+	}
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.contentAmount}>
+				<Text style={styles.amount}>{data.amount}</Text>
+			</View>
+
+			<View style={styles.contentName}>
+				<Text style={styles.name}>{data.name}</Text>
+			</View>
+
+			<TouchableOpacity onPress={handleDeleteItem}>
+				<Feather name="trash-2" color="#F00" size={25} />
+			</TouchableOpacity>
+		</View>
+	)
+}
